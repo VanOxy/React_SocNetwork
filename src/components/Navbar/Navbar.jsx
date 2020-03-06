90cd2c131af25a455bdf087b106fb5633731d247
Ivan <ivanokulichev@gmail.com>
2020-03-06 03:29:27 +0100

    active link highlight

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import  React  from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = (props) => {
     return (
         <div className={s.nav}>
-            <div><a href="/profile">Profile</a></div>
-            <div><a href="/dialogs">Dialogs</a></div>
-            <div><a href="/">Photos</a></div>
-            <div><a href="/">Videos</a></div>
+            <div>
+                <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+            </div>
+            <div>
+                <NavLink to="/dialogs" activeClassName={s.active}>Dialogs</NavLink>
+            </div>
+            <div>
+                <a href="/">Photos</a>
+            </div>
+            <div>
+                <a href="/">Videos</a>
+            </div>
         </div>
     );
 }
